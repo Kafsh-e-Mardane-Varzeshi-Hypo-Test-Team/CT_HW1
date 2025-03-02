134aa5bf730bfcb2d829e64a6d7e42977359a13a
mammedbrk <mammedbrk@gmail.com>
2025-03-02 11:30:33 +0330

    feat: add Queue struct fields

--- internal/queue.go
+++ internal/queue.go
@@ -1,4 +1,12 @@
 package internal
 
 type Queue struct {
+	Name            string
+	downloads       []*Download
+	savePath        string
+	maxConcurrent   int
+	activeDownloads int
+	maxBandwidth    int
+	maxRetries      int
+	activeHours     string
 }
