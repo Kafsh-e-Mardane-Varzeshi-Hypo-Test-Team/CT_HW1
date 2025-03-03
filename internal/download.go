d2811ce5d4f0ac1e8623965cf1c84d107831f9e3
Parisa Mousavi <parisa.mousavi@divar.ir>
2025-03-03 20:13:57 +0330

    feat: implement func SupportsPartialDownload() in Download struct

--- internal/download.go
+++ internal/download.go
@@ -1,5 +1,11 @@
 package internal
 
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
 type Status int
 
 const (
@@ -16,4 +22,34 @@ type Download struct {
 	OutputFileName string
 	Queue          *Queue
 	Status
+	contentLength int64
+}
+
+func (d *Download) SupportsPartialDownload() bool {
+	req, err := http.NewRequest("HEAD", d.URL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("Failed to get response from server")
+		return false
+	}
+
+	d.contentLength = resp.ContentLength
+	if resp.Header.Get("Accept-Ranges") == "" || resp.Header.Get("Accept-Ranges") == "none" {
+		log.Fatal("Server does not support partial download")
+		return false
+	}
+
+	return true
 }
