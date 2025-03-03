79430f79edcd00b52424e2f9c9ac2131cc49df53
Parisa Mousavi <parisa.mousavi@divar.ir>
2025-03-03 22:09:23 +0330

    feat: implement Start() and related internal methods in Download struct

--- internal/download.go
+++ internal/download.go
@@ -66,3 +66,73 @@ func (d *Download) supportsPartialDownload() bool {
 
 	return true
 }
+
+func (d *Download) downloadThisPart(startIndex, endIndex int) {
+	req, err := http.NewRequest("GET", d.URL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rangeOfDownload := strconv.Itoa(startIndex) + "-" + strconv.Itoa(endIndex)
+	rangeHeader := "bytes=" + rangeOfDownload
+	req.Header.Set("Range", rangeHeader)
+
+	// TODO: Ask if it's better to save this client as a field in Download struct
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Write the response to file
+	file, err := os.Create(d.Destination + "/" + d.OutputFileName + rangeOfDownload + ".part")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Downloaded ", rangeOfDownload)
+	}
+}
+
+func (d *Download) downloadInParts() {
+	numberOfParts := 2
+	partSize := d.contentLength / numberOfParts
+
+	for i := 0; i < numberOfParts; i++ {
+		startIndex := i * partSize
+		endIndex := (i + 1) * partSize
+		if (i == numberOfParts - 1) {
+			endIndex = d.contentLength - 1
+		}
+		d.downloadThisPart(startIndex, endIndex)
+	}
+}
+
+func (d *Download) downloadInOneGo() {
+	d.downloadThisPart(0, d.contentLength - 1)
+}
+
+func (d *Download) Start() {
+	d.setHttpResponse()
+	d.setContentLength()
+
+	if d.contentLength == 0 {
+		log.Fatal("Content length is invalid")
+	} else {
+		d.Status = InProgress
+		log.Println("Content length is", d.contentLength)
+		if d.supportsPartialDownload() {
+			d.downloadInParts()
+		} else {
+			d.downloadInOneGo()
+		}
+	}
+}
