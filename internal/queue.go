eaf2281572f177614b7f2108f2ed48a742a65477
mammedbrk <mammedbrk@gmail.com>
2025-03-05 11:50:08 +0330

    feat: add NewQueue function

--- internal/queue.go
+++ internal/queue.go
@@ -1,20 +1,27 @@
 package internal
 
+import "sync"
+
 type Queue struct {
 	Name            string
 	downloads       []*Download
-	savePath        string
-	maxConcurrent   int
+	SavePath        string
+	MaxConcurrent   int
 	activeDownloads int
-	maxBandwidth    int
-	maxRetries      int
-	activeHours     string
+	MaxBandwidth    int
+	MaxRetries      int
+	ActiveHours     string
+	// ch              chan int
+	mu sync.Mutex
 }
 
-func (queue *Queue) AddDownload(download *Download) {
-	queue.downloads = append(queue.downloads, download)
-	if queue.activeDownloads < queue.maxConcurrent {
-		queue.activeDownloads++
-		// Start download
+func NewQueue(name string, savePath string, maxConcurrent int, maxBandwidth int, maxRetries int, activeHours string) *Queue {
+	return &Queue{
+		Name:          name,
+		SavePath:      savePath,
+		MaxConcurrent: maxConcurrent,
+		MaxBandwidth:  maxBandwidth,
+		MaxRetries:    maxRetries,
+		ActiveHours:   activeHours,
 	}
 }
