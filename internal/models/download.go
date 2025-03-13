68252119ae1e202e4fc97f0e18592f954acf501f
Mefi <Mefi.Taheri@gmail.com>
2025-03-14 03:18:51 +0330

    feat: add DownloadsTab

--- internal/models/download.go
+++ internal/models/download.go
@@ -1,6 +1,68 @@
 package models
 
+// {Title: "URL", Width: 30},
+// {Title: "Queue", Width: 15},
+// {Title: "Status", Width: 10},
+// {Title: "Transfer Rate", Width: 15},
+// {Title: "Progress", Width: 10},
 type Download struct {
-	Name     string
-	Progress float64
+	Url          string
+	Queue        string
+	Status       string
+	TransferRate string
+	Progress     float64
+}
+
+func GetDownloads() []Download {
+	return []Download{
+		{
+			Url:          "https://example.com",
+			Queue:        "1",
+			Status:       "Downloading",
+			TransferRate: "1MB/s",
+			Progress:     0.5,
+		},
+		{
+			Url:          "https://downloadha.com",
+			Queue:        "2",
+			Status:       "Downloading",
+			TransferRate: "2MB/s",
+			Progress:     0.8,
+		},
+		{
+			Url:          "https://google.com",
+			Queue:        "3",
+			Status:       "Downloading",
+			TransferRate: "1.5MB/s",
+			Progress:     0.2,
+		},
+		{
+			Url:          "https://youtube.com",
+			Queue:        "4",
+			Status:       "Paused",
+			TransferRate: "",
+			Progress:     0.1,
+		},
+		{
+			Url:          "https://facebook.com",
+			Queue:        "5",
+			Status:       "Completed",
+			TransferRate: "",
+			Progress:     1,
+		},
+		{
+			Url:          "https://instagram.com",
+			Queue:        "6",
+			Status:       "Error",
+			TransferRate: "",
+			Progress:     0,
+		},
+		{
+			Url:          "https://twitter.com",
+			Queue:        "7",
+			Status:       "Downloading",
+			TransferRate: "0.5MB/s",
+			Progress:     0.7,
+		},
+	}
 }
--- internal/tui/downloads_tab.go
+++ internal/tui/downloads_tab.go
@@ -1,24 +1,93 @@
 package tui
 
 import (
+	"fmt"
+
 	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW1/internal/models"
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
 
 type DownloadsTab struct {
 	Downloads []models.Download
+	table     table.Model
 }
 
 func NewDownloadsTab() DownloadsTab {
-	return DownloadsTab{}
+	Downloads := models.GetDownloads()
+	columns := []table.Column{
+		{Title: "URL", Width: 30},
+		{Title: "Queue", Width: 15},
+		{Title: "Status", Width: 15},
+		{Title: "Transfer Rate", Width: 15},
+		{Title: "Progress", Width: 10},
+	}
+	rows := []table.Row{}
+	for _, download := range Downloads {
+		rows = append(rows, []string{
+			download.Url,
+			download.Queue,
+			download.Status,
+			download.TransferRate,
+			fmt.Sprintf("%#6.2f%%", download.Progress*100),
+		})
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(10),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(true)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	return DownloadsTab{
+		Downloads: Downloads,
+		table:     t,
+	}
 }
 
 func (m DownloadsTab) Init() tea.Cmd { return nil }
 
 func (m DownloadsTab) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	// case tea.WindowSizeMsg:
+	// 	m.table.SetHeight(msg.Height - 3)
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		}
+	}
+
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
 }
 
 func (m DownloadsTab) View() string {
-	return "Downloads Tab"
+	// TODO: add help text
+	return baseStyle.Render(m.table.View()) + "\n"
 }
