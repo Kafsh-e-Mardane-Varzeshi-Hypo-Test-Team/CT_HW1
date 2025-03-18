e4b6bdb1276c13510d1e241cdc4e751797120ab6
mammedbrk <mammedbrk@gmail.com>
2025-03-18 23:28:37 +0330

    feat: implement addQueue and removeQueue methods

--- internal/manager.go
+++ internal/manager.go
@@ -50,7 +50,7 @@ func (m *Manager) removeDownload(d *Download) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	d.Stop()
+	d.Stop() // TODO: error handling
 
 	for i, download := range m.downloads {
 		if download == d {
@@ -60,5 +60,34 @@ func (m *Manager) removeDownload(d *Download) error {
 	}
 
 	log.Printf("removed download %q from queue %q\n", d.URL, d.GetQueueName())
+	return nil
+}
+
+func (m *Manager) addQueue(q *Queue) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, exists := m.queues[q.name]; exists {
+		return errors.New("queue already exists")
+	}
+
+	m.queues[q.name] = q
+	log.Printf("Manager: Added queue %q\n", q.name)
+	return nil
+}
+
+func (m *Manager) removeQueue(queueName string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	q, exists := m.queues[queueName]
+	if !exists {
+		return errors.New("queue does not exist")
+	}
+
+	q.Stop() // TODO: error handling
+
+	delete(m.queues, queueName)
+	log.Printf("Manager: Removed queue %q\n", queueName)
 	return nil
 }
