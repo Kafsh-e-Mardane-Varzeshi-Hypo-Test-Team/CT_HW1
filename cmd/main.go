a0d1fdd251baaea4d0280f46da0aedaacdc938aa
Mefi <Mefi.Taheri@gmail.com>
2025-03-20 19:05:35 +0330

    fix: delete extra app.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW1/internal/app"
+	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW1/internal/tui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(app.New())
+	p := tea.NewProgram(tui.NewMainView())
 	if _, err := p.Run(); err != nil {
 		panic(err)
 	}
