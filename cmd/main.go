22943895ffeaf81801f273648011a0b5cb01dd18
Mefi <Mefi.Taheri@gmail.com>
2025-03-21 16:26:07 +0330

    debug: add debug downloads

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,41 @@ import (
 
 func main() {
 	manager := models.NewManager()
+	manager.Queues = map[string]*models.Queue{
+		"Queue1": {
+			Name: "Queue1",
+		},
+	}
+	manager.Downloads = []*models.Download{
+		{
+			URL:          "https://speed.hetzner.de/100MB.bin",
+			Queue:        "Queue1",
+			Status:       models.InProgress,
+			TransferRate: 123,
+			Progress:     0.234,
+		},
+		{
+			URL:          "https://speed.hetzner.de/100MB.bin",
+			Queue:        "Queue1",
+			Status:       models.InProgress,
+			TransferRate: 123123,
+			Progress:     0.134,
+		},
+		{
+			URL:          "https://speed.hetzner.de/100MB.bin",
+			Queue:        "Queue1",
+			Status:       models.InProgress,
+			TransferRate: 123123123,
+			Progress:     0.334,
+		},
+		{
+			URL:          "https://speed.hetzner.de/100MB.bin",
+			Queue:        "Queue1",
+			Status:       models.InProgress,
+			TransferRate: 123123123123,
+			Progress:     0.634,
+		},
+	}
 	p := tea.NewProgram(tui.NewMainView(manager))
 	if _, err := p.Run(); err != nil {
 		panic(err)
