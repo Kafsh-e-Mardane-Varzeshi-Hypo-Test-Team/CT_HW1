59c9a4a80ea9f999ca42ef9e79e7605ef5e53a88
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-03-21 19:09:17 +0330

    fix: change directory of logfile

--- internal/logger/log.go
+++ internal/logger/log.go
@@ -6,7 +6,7 @@ import (
 )
 
 func StartLoggingToFile() {
-	file, err := os.OpenFile("logfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile("internal/logger/logfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Printf("error opening file: %v", err)
 	}
